2e7d4599c58ac785b52ae157df05f12326101c75
Vadim <manavar2017@yandex.com>
2022-11-21 21:52:42 +0400

    Initial commit

---
+++ src/main/java/com/imdbsecondedition/ImdbSecondEditionApplication.java
@@ -0,0 +1,12 @@
+package com.imdbsecondedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbSecondEditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImdbSecondEditionApplication.class, args);
+    }
+}
