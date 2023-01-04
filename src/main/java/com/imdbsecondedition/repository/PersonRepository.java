f1e11625712d9a85368c57f2272f10e0674ccaf8
Vadim <manavar2017@yandex.com>
2023-01-04 18:44:40 +0400

    Ui branch initialization

--- src/main/java/com/imdbsecondedition/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imdbsecondedition.repository;
-
-import com.imdbsecondedition.model.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
