1447c9fd9b5372474349caf5461a76f4fa843adb
Vadim <manavar2017@yandex.com>
2023-01-04 18:47:05 +0400

    Clean master

--- src/main/java/com/imdbsecondedition/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imdbsecondedition.repository;
-
-import com.imdbsecondedition.model.Country;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends CrudRepository<Country, Long> {
-}
--- src/main/java/com/imdbsecondedition/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imdbsecondedition.repository;
-
-import com.imdbsecondedition.model.Review;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends CrudRepository<Review, Long> {
-}
